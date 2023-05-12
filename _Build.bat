4afc3616aa9a6c97f320af07409f24ccb7512b9c
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2023-05-12 15:03:59 +0930

    Add files via upload

---
+++ _Build.bat
@@ -0,0 +1,4 @@
+call NHAEFIPkg\_Build_.bat
+copy C:\edk2\Build\NHA_EFI\RELEASE_VS2019\X64\NHA_EFI.efi C:\edk2\NHAEFIPkg\NHA_EfiDSE_UI\NHADSE\EFI\NHA_EFI.efi
+copy C:\edk2\Build\NHA_EFI\RELEASE_VS2019\X64\Loader.efi C:\edk2\NHAEFIPkg\NHA_EfiDSE_UI\NHADSE\EFI\bootx64.efi
+pause
---
+++ _Build_.bat
@@ -0,0 +1,3 @@
+cd C:\edk2
+call edksetup
+build -a X64 -t VS2019 -p NHAEFIPkg/NHAEFIPkg.dsc -b RELEASE
